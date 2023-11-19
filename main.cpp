c7f0e8b8274f9f470e687ec89e7cd9f506497ddd
JosePeblo <a01275676@tec.mx>
2023-11-18 20:27:20 -0600

    sad

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include <vector>
 #include <set>
 #include <sstream>
+#include <limits>
+#include <set>
 #include <stdexcept>
 #include "cmlmatrix.h"
+const int INF = std::numeric_limits<int>::max();
 
 typedef cml::matrix<int> i32Mat;
 typedef std::vector<int> i32Vec;
@@ -69,6 +72,8 @@ void TSP(cml::matrix<int>& graph) {
     }
 
     if(bestPathIndex > -1) {
+        std::cout << "El recorrido:" << std::endl;
+
         auto bestPath = paths[bestPathIndex];
         for(int i = 0; i < bestPath.path.size(); i++) {
             std::cout << bestPath.path[i] + 1;
@@ -76,8 +81,11 @@ void TSP(cml::matrix<int>& graph) {
                 std::cout << " -> ";
             }
         }
+
         std::cout << std::endl;
-        std::cout << bestPath.weigth << std::endl;
+        std::cout << "El costo: " << bestPath.weigth << std::endl;
+    } else {
+        std::cout << "Error: no hay un mejor camino" << std::endl;
     }
 
 }
@@ -91,6 +99,32 @@ void stringToPair(std::string& str, std::pair<int, int>& pr) {
     pr.first = std::stoi(token);
     std::getline(ss, token, ',');
     pr.second = std::stoi(token);
+}
+
+void dijkstra(cml::matrix<int>& matrix, int originNode, int numNode){
+    std::vector<int> distance(numNode, INF);
+    distance[originNode] = 0;
+    std::set<std::pair<int, int>> priorityQueue;
+    priorityQueue.insert({0,originNode});
+
+    while (!priorityQueue.empty()){
+        int actualNode = priorityQueue.begin()->second;
+        priorityQueue.erase(priorityQueue.begin());
+        for (int neighbourNode = 0; neighbourNode < numNode; ++neighbourNode){
+            if(matrix[actualNode][neighbourNode] != INF){
+                int newDistance = distance[actualNode] + matrix[actualNode][neighbourNode];
+                if (newDistance < distance[neighbourNode]){
+                    priorityQueue.erase({distance[neighbourNode],neighbourNode});
+                    distance[neighbourNode] = newDistance;
+                    priorityQueue.insert({distance[neighbourNode], neighbourNode});
+                }
+            }
+        }
+    }
+    for(int i = 0; i < numNode; ++i){
+        if (i == originNode) continue;
+        std::cout<<"Km de colonia "<< originNode + 1 << " a colonia " << i + 1 << ": " << distance[i]<<"\n";
+    }
 }
 
 int main(void) {
@@ -130,10 +164,16 @@ int main(void) {
 
     // 1.- Forma óptima de cablear la fibra óptica conenctando colonias
     // para que se pueda compartir la infotmación cualesquiera dos colonias
+    std::cout<<"Punto 01"<<std::endl;
+    for (int i = 0; i < numNodes; ++i) {
+        dijkstra(colonies,i,numNodes);
+        std::cout<<"\n";
+    }
 
     // Borren esto, ocupen la mtríz de adyacencia "colonies[i][j]" y así 
 
     // 2.- Ruta más corta posible para visitar cada colonia una sola vez
+    std::cout << "Punto 02" << std::endl;
     TSP(colonies);
 
     // 3.- 
